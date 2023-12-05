251edca6529a5ee6aecbb29aef96a4f588b28b9e
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-12-06 00:33:40 +0530

    title fixed

--- src/pages/stream/index.jsx
+++ src/pages/stream/index.jsx
@@ -8,16 +8,20 @@ import {
   Modal,
   TextInput,
   Textarea,
+  Card,
 } from "flowbite-react";
+
+import { v5 as uuidv5 } from "uuid";
+import Link from "next/link";
 
 export default function Stream() {
   const [streamCreated, setStreamCreated] = useState(false);
-  const [streamName, setStreamName] = useState("");
+  const [streamTitle, setStreamTitle] = useState("");
   const [streamDescription, setStreamDescription] = useState("");
   const [streamThumbnailUrl, setStreamThumbnailUrl] = useState("");
   const [streamUrl, setStreamUrl] = useState("");
   const [openModal, setOpenModal] = useState(false);
-  const titleRef = useRef(null)
+  const titleRef = useRef(null);
 
   function onCloseModal() {
     setOpenModal(false);
@@ -26,15 +30,47 @@ export default function Stream() {
   return (
     // min-h-[calc(100vh-5rem)]
 
-    <main className=" min-h-[calc(100vh-5rem)] flex flex-col pt-14 justify-center">
+    <main className="min-h-[calc(100vh-4rem)] flex flex-col pt-14 ">
       {streamCreated ? (
-        <div>Created stream</div>
+        <div className=" border-2  border-red-500 grid self-center gap-x-20 gap-y-10   grid-cols-3 py-12 px-4">
+          <Card
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            // imgSrc="/images/blog/image-1.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {streamTitle}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {streamDescription}
+            </p>
+            <Button href={streamUrl} > Go Live! </Button>
+          </Card>
+          <Card
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            // imgSrc="/images/blog/image-1.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <Link href={streamUrl}><Button > Go Live! </Button></Link>
+          </Card>
+
+        </div>
       ) : (
-        <div className="flex flex-col items-center gap-3">
+        <div className="flex flex-col h-[calc(100vh-6.5rem)] justify-center  border-2 items-center gap-3">
           <div className="text-lg">
             {"You haven't created any streams yet :("}{" "}
           </div>
-          <Button className="!bg-rose-700" onClick={() => setOpenModal(true)}>
+          <Button
+            className="!bg-rose-700 !outline-none  !ring-0 hover:!bg-rose-800"
+            onClick={() => setOpenModal(true)}
+          >
             Create Stream
           </Button>
           <Modal
@@ -43,7 +79,7 @@ export default function Stream() {
             size="md"
             onClose={onCloseModal}
             popup
-            className="!"
+            className="!border-none !outline-none !ring-0"
             initialFocus={titleRef}
           >
             <Modal.Header className="!bg-slate-800 rounded-t-lg" />
@@ -59,7 +95,8 @@ export default function Stream() {
                   <TextInput
                     id="Title"
                     placeholder="IRL Streaming"
-                    value={""}
+                    value={streamTitle}
+                    onChange={(event) => setStreamTitle(event.target.value)}
                     ref={titleRef}
                     // onChange={(event) => setEmail(event.target.value)}
                     required
@@ -73,6 +110,11 @@ export default function Stream() {
                     id="description"
                     className=" focus:!border-white focus:!ring-white"
                     placeholder="Streaming live from the hacker house"
+                    value={streamDescription}
+                    onChange={(event) =>
+                      setStreamDescription(event.target.value)
+                    }
+                    required
                   />
                 </div>
 
@@ -80,24 +122,46 @@ export default function Stream() {
                   <div className="mb-2 block">
                     <Label htmlFor="Thumbnail URL" value="Thumbnail URL" />
                   </div>
-                  <TextInput id="Thumbnail URL" placeholder="https://" />
+                  <TextInput
+                    id="Thumbnail URL"
+                    placeholder="https://"
+                    value={streamThumbnailUrl}
+                    onChange={(event) =>
+                      setStreamThumbnailUrl(event.target.value)
+                    }
+                    required
+                  />
                 </div>
 
                 <div>
                   <div className="mb-2 block">
                     <Label htmlFor="Stream URL" value="Stream URL" />
                   </div>
-                  <TextInput id="Stream URL"  placeholder="https://" />
+                  <TextInput
+                    id="Stream URL"
+                    placeholder="https://"
+                    value={streamUrl}
+                    onChange={(event) => setStreamUrl(event.target.value)}
+                    required
+                  />
                 </div>
-
-
 
                 <div className="w-full  ">
-                  <Button className="!bg-rose-700 !outline-none  !ring-0 hover:!bg-rose-800" onClick={()=>{
-                    onCloseModal()
-                  }}>Create!</Button>
+                  <Button
+                    className="!bg-rose-700 !outline-none  !ring-0 hover:!bg-rose-800"
+                    onClick={() => {
+                      // const id =uuidv5()
+                      if(streamTitle==="" || streamDescription=="" ||streamThumbnailUrl==""||streamUrl==""  ){
+                        window.alert("Please fill out all the fields")
+                        return
+                      }
+                      onCloseModal();
+                      setStreamCreated(true);
+                    }}
+                  >
+                    Create!
+                  </Button>
                 </div>
-
               </div>
             </Modal.Body>
           </Modal>
