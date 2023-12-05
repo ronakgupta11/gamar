847ef132221ee7a1384f630e11c6cb76798b25ce
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-12-05 13:57:46 +0530

    init

---
+++ src/pages/_app.jsx
@@ -0,0 +1,37 @@
+import "@/styles/globals.css";
+import { SiteHeader } from "@/components/site-header";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ArweaveWalletKit } from "arweave-wallet-kit";
+import { Footer } from "@/components/footer";
+import { Toaster } from "@/components/ui/toaster";
+import { UserProvider } from "@/hooks/useUser";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ArweaveWalletKit
+        config={{
+          permissions: ["ACCESS_ADDRESS", "SIGN_TRANSACTION", "DISPATCH"],
+          ensurePermissions: true,
+          appInfo: {
+            name: "StarterKit",
+          },
+        }}
+      >
+        <UserProvider>
+          <div className="relative flex min-h-screen flex-col">
+            <SiteHeader />
+            <div className="flex-1">
+              <Component {...pageProps} />
+            </div>
+            <Toaster />
+            <Footer />
+          </div>
+
+          <TailwindIndicator />
+        </UserProvider>
+      </ArweaveWalletKit>
+    </ThemeProvider>
+  );
+}
