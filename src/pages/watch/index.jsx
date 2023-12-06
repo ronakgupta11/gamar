6d5895a2b70384c81c8f9f3927edf3e2ecb753d2
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-12-06 10:53:52 +0530

    almost done?

--- src/pages/watch/index.jsx
+++ src/pages/watch/index.jsx
@@ -1,39 +1,35 @@
-import { useStream } from '@livepeer/react';
-import { Player,Broadcast } from '@livepeer/react';
-import { useRouter } from 'next/router';
-
-
-    
-
-
-
+import { useStream } from "@livepeer/react";
+import { Player, Broadcast } from "@livepeer/react";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 function WatchStream() {
+  const router = useRouter();
+  const id =(router.query.streamId);
 
-    const router = useRouter()
-    const id  = router.query.streamId
-    const title = router.query.title
-    console.log(id, title)
-    const {data:asset,isError,error} = useStream(id)
+  const title = router.query.title;
+  console.log(id, title);
 
-    // const {id} =  useLoaderData();
-    // const {data:asset,isError,error} = useStream(id)
-    // console.log(asset)
+  if (id) {
+    const { data: asset, isError, error } = useStream(id);
+    console.log(asset);
 
-
-  return (
-    <div className='mt-16 border-white border-2'>
+    return (
+      <div className="mt-16 border-white border-2">
         <Player
-        title={asset?.name}
-        playbackId={asset?.playbackId}
+            title={asset?.name}
+            playbackId={asset?.playbackId}
+    
+            
+            />
+      </div>
+    );
+  }
+  // const {data:asset,isError,error} = useStream(id)
+  // console.log(asset)
+  // const {id} =  useLoaderData();
 
-        
-        />
-
-        
-    </div>
-  )
+  return "";
 }
 
-
-export default WatchStream
+export default WatchStream;
